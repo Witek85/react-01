62304a20c13767aac9e7cf326ece0460edf54e4a
Witek85 <witold.szklarski@gmail.com>
2018-06-11 23:41:06 +0200

    component

---
+++ src/first.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class First extends Component {
+    render() {
+        return (
+            <p>Hello world!</p>
+        );
+    }
+}
+
+// Must export!
+export default First;
