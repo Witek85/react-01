defb7b3b8dd3e87c9b5af15ca0875bba159041bc
Witek85 <witold.szklarski@gmail.com>
2018-06-13 00:43:30 +0200

    events

--- src/first.jsx
+++ src/first.jsx
@@ -6,8 +6,12 @@ class First extends Component {
         return (
             <p className="First-intro">
                 Hello {this.props.name} ! To get started, edit <code>src/App.js</code> and save to reload.
+                <button className="btn btn-default" onClick={this.onClickHandler}>Click!</button>;
             </p>
         );
+    }
+    onClickHandler() {
+        console.log("click!");
     }
 }
 
