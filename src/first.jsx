ce7c4ad820da376196176c5c0de5274b6f35e977
Witek85 <witold.szklarski@gmail.com>
2018-06-17 00:28:05 +0200

    'oninput'

--- src/first.jsx
+++ src/first.jsx
@@ -6,17 +6,30 @@ class First extends Component {
         super();
         this.state = {
             counter: 0,
-            text: "not clicked"
+            text: "not clicked",
+            firstName: "1",
+            lastName: "2",
         };
     }
     render() {
         return (
-            <p className="First-intro">
+            <div className="First-intro">
                 Hello {this.props.name} ! To get started, edit <code>src/App.js</code> and save to reload.
                 <button className="btn btn-default" onClick={this.onClickHandler}>Click!</button>;
                 <p>{this.state.counter}</p>
                 <p>{this.state.text}</p>
-            </p>
+                <form class="form-inline" action="/action_page.php">
+                    <div className="form-group">
+                        <label htmlFor="firstName">Imię</label>
+                        <input type="text" className="form-control" id="firstName" value={this.state.firstName} onInput={this.changeFirstName} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="lastName">Imię</label>
+                        <input type="text" className="form-control" id="lastName" value={this.state.lastName} onInput={this.changeLastName} />
+                    </div>
+                </form>
+                <output>{this.state.firstName}&nbsp;{this.state.lastName}</output>
+            </div>
         );
     }
     onClickHandler = () => {
@@ -25,6 +38,12 @@ class First extends Component {
             counter: this.state.counter + 1,
             text: "clicked!"
           })
+    }
+    changeFirstName = (e) => {
+        this.setState({firstName: e.target.value});
+    }
+    changeLastName = (e) => {
+        this.setState({lastName: e.target.value});
     }
     
 }
