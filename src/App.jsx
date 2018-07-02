a9ff9f7e7db918c93b738452c69abd8a20662c34
Witek85 <witold.szklarski@gmail.com>
2018-07-02 13:19:41 +0200

    fake routing

--- src/App.jsx
+++ src/App.jsx
@@ -8,38 +8,43 @@ import ThirdParent from './Third_parent';
 import Api1 from './Api1';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        currentPage: "Products"
+    };
+  }
   render() {
     return (
       <main className="App">
-        <Header />
-        <section>
-          <div className="container">
-            <Products />
-          </div>
-        </section>
-        <section>
-          <div className="container">
-            <Second title="this is title" content="this is content" />
-          </div>
-        </section>
-        <section>
-          <div className="container">
-            <First name="World" />
-          </div>
-        </section>
-        <section>
-          <div className="container">
-            <ThirdParent />
-          </div>
-        </section>
-        <section>
-          <div className="container">
-            <Api1 />
-          </div>
-        </section>
+        <Header changePage={this.changePage} />
+        {this.renderContent()}
       </main>
     );
   }
+  renderContent() {
+    if (this.state.currentPage === "Products") {
+      return (
+        <section>
+        <div className="container">
+          <Products />
+        </div>
+      </section>
+      )
+    } else if (this.state.currentPage === "First") {
+      return (<section><div className="container"><First name="World" /></div></section>)
+    } else if (this.state.currentPage === "Second") {
+      return (<section><div className="container"><Second title="this is title" content="this is content" /></div></section>)
+    } else if (this.state.currentPage === "ThirdParent") {
+      return (<section><div className="container"><ThirdParent /></div></section>)
+    } else if (this.state.currentPage === "Api1") {
+      return (<section><div className="container"><Api1 /></div></section>)
+    }
+  }
+  changePage = (page) => {
+    this.setState({ currentPage: page })
+    console.log('currentPage: ', page);
+}
 }
 
 export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
-import Navbar from './Navbar';
 
 class Header extends Component {
     render() {
+        const links = [
+            { url: '#', text: 'Products', page:'Products' },
+            { url: '#', text: 'First', page:'First' },
+            { url: '#', text: 'Second', page:'Second' },
+            { url: '#', text: 'ThirdParent', page:'ThirdParent' },
+            { url: '#', text: 'Api1', page:'Api1' }
+        ];
         return (
             <nav className="navbar navbar-default navbar-static-top">
                 <div className="container">
@@ -16,12 +22,24 @@ class Header extends Component {
                     <a className="navbar-brand" href="#">Welcome to React</a>
                 </div>
                     <div id="navbar" className="navbar-collapse collapse">
-                        <Navbar />
+                        <ul className="nav navbar-nav">{links.map(this.renderLink)}</ul>
                     </div>
                 </div>
           </nav>
-        );
+        ) 
     }
+    renderLink = ({url, text, page}) => {
+        return <li key={text}>
+            <a href={url} className="my-awesome-link" onClick={()=>this.props.changePage(page)} >
+                {text}
+            </a>
+            </li>; 
+    }
+    // handleClick = (text) => {
+    //     console.log('clicked link with text: ', text);
+    // }
 }
 
 export default Header;
+
+// onClick={() =>this.handleClick(text)}
