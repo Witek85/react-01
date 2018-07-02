bdb088f00d4076a8ac467baa8972010a33e630eb
Witek85 <witold.szklarski@gmail.com>
2018-07-02 09:56:57 +0200

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import First from './First'
 import Second from './Second'
 import './App.css';
-import Navbar from './navbar';
+import Header from './Header';
 import ThirdParent from './Third_parent';
 import Api1 from './Api1';
 
@@ -10,15 +10,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <Header />
         <section>
           <div className="container">
-            <First name="World" />
+            <Second title="this is title" content="this is content" />
           </div>
         </section>
         <section>
           <div className="container">
-            <Second title="this is title" content="this is content" />
+            <First name="World" />
           </div>
         </section>
         <section>
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,30 +1,27 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-default navbar-static-top">
-                <div className="container">
-                <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span className="sr-only">Toggle navigation</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    </button>
-                    <a className="navbar-brand" href="#">Welcome to React</a>
-                </div>
-                <div id="navbar" className="navbar-collapse collapse">
-                    <ul className="nav navbar-nav">
-                    <li className="active"><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    </ul>
-                </div>
-                </div>
-          </nav>
-        );
+    render() {         
+        return React.createElement('ul', {className: 'app-menu nav navbar-nav'}, [
+            React.createElement('li', {className: 'app-menu_item'}, [
+            React.createElement('a', {href: 'https://someurl'}, ['First link'])
+            ]),
+            React.createElement('li', {className: 'app-menu_item'}, [
+            React.createElement('a', {href: 'https://someurl'}, ['Second link'])
+            ])
+        ]);   
     }
 }
 
 export default Navbar;
+
+// return (
+//     React.createElement('ul', {})
+//                     <div id="navbar" className="navbar-collapse collapse">
+//             <ul className="nav navbar-nav">
+//             <li className="active"><a href="#">Home</a></li>
+//             <li><a href="#about">About</a></li>
+//             <li><a href="#contact">Contact</a></li>
+//             </ul>
+//         </div>
+// );
