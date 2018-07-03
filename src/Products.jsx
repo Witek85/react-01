6db14205aa70b807ff77eeede98049e5813433e3
Witek85 <witold.szklarski@gmail.com>
2018-07-03 10:05:44 +0200

    redux works!

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import Product from './Product'
+import { connect } from 'react-redux';
 import './Products.css';
+import { filterProducts } from './actions/products';
+
+function mapStateToProps(state) {
+  return {
+    filteredProducts: state.products.filteredProducts
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        filterProducts: query => dispatch(filterProducts(query))
+    }
+}
 
 class Products extends Component {
     constructor() {
@@ -10,17 +24,19 @@ class Products extends Component {
         };
     }
     render() {
-        const products = [
-            { id: 1, price: 1000, description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.' },
-            { id: 2, price: 2000, description: 'Lorem ipsum dolor sit amet lorem. Integer mi metus, ullamcorper tellus et dui. Suspendisse egestas imperdiet sapien. In pede.  Curabitur et ligula.' },
-            { id: 3, price: 500, description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.' },
-            { id: 4, price: 1500, description: 'Lorem ipsum dolor sit amet lorem. Integer mi metus, ullamcorper tellus et dui. Suspendisse egestas imperdiet sapien. In pede.  Curabitur et ligula.' },
-            { id: 5, price: 750, description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.' },
-        ];
+        // const products = [
+        //     { id: 1, price: 1000, description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.' },
+        //     { id: 2, price: 2000, description: 'Lorem ipsum dolor sit amet lorem. Integer mi metus, ullamcorper tellus et dui. Suspendisse egestas imperdiet sapien. In pede.  Curabitur et ligula.' },
+        //     { id: 3, price: 500, description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.' },
+        //     { id: 4, price: 1500, description: 'Lorem ipsum dolor sit amet lorem. Integer mi metus, ullamcorper tellus et dui. Suspendisse egestas imperdiet sapien. In pede.  Curabitur et ligula.' },
+        //     { id: 5, price: 750, description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.' },
+        // ];
+        console.log(this.props.filteredProducts);
+        const allProducts = this.props.filteredProducts;
         return (
             <div>
                 <p>{this.state.orderSum}</p>
-                <div className="App-content">{products.map(this.renderProduct)}</div>
+                <div className="App-content">{allProducts.map(this.renderProduct)}</div>
             </div>
         )
     }
@@ -37,4 +53,5 @@ class Products extends Component {
     }
 }
 
-export default Products;
+// export default Products;
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
--- src/data.js
+++ src/data.js
@@ -1,6 +1,14 @@
-const allProducts = [1, 2, 3, 4, 5].map(item => ({
-    title: `Item No ${item}`,
-    price: 30 + item * 5
-  }));
+// const allProducts = [1, 2, 3, 4, 5].map(item => ({
+//     title: `Item No ${item}`,
+//     price: 30 + item * 5
+//   }));
+
+const products = [
+    { id: 1, price: 4000, description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.' },
+    { id: 2, price: 2000, description: 'Lorem ipsum dolor sit amet lorem. Integer mi metus, ullamcorper tellus et dui. Suspendisse egestas imperdiet sapien. In pede.  Curabitur et ligula.' },
+    { id: 3, price: 500, description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.' },
+    { id: 4, price: 1500, description: 'Lorem ipsum dolor sit amet lorem. Integer mi metus, ullamcorper tellus et dui. Suspendisse egestas imperdiet sapien. In pede.  Curabitur et ligula.' },
+    { id: 5, price: 750, description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.' },
+];
   
-  export default allProducts;
+export default products;
