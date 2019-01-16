0eb6926de282d9e8d30d42180630c26ebc9fe94e
Witek85 <witold.szklarski@gmail.com>
2019-01-17 00:53:02 +0100

    api1

--- src/components/api1/Api1.jsx
+++ src/components/api1/Api1.jsx
@@ -3,25 +3,30 @@ import React, { Component } from 'react';
 class Api1 extends Component {
 
     state = {
-        contacts: []
+        movies: []
     };
-    
     componentDidMount() {
         fetch('https://facebook.github.io/react-native/movies.json')  
         .then((response) => response.text())
         .then((responseText) => {
-            console.log(JSON.parse(responseText));
+            console.log(responseText);
+            const resText = JSON.parse(responseText);
+            this.setState({movies: resText.movies})
         })
         .catch((error) => {
             console.log("reset client error-------",error);
         });     
     };
-
-
     render() {
+        const movies = this.state.movies.map(key => {
+            console.log(key);
+            return (
+                <div>{key.id} - {key.title} ({key.releaseYear})</div>
+            )
+        });
         return (
             <div>
-            API1
+                {movies}
             </div>
             )
     }
