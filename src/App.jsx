dd28cc6c3a1e41bc4bd0721c16fba837dcfc6750
Witek85 <witold.szklarski@gmail.com>
2019-02-09 13:29:44 +0100

    react lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 // import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
@@ -12,11 +12,13 @@ import Api1 from './components/api1/Api1';
 import TodoApp from './components/todoapp/TodoApp';
 import InputOutput from './components/inputOutput/inputOutput';
 import Lists from './components/lists/lists';
-import users from './components/users/users';
+// import users from './components/users/users';
 import courses from './components/courses/courses';
 import course from './components/courses/course/course';
 import iss from './components/iss/iss';
 import { fetchProducts } from "./actions/products";
+
+const users = lazy(() => import('./components/users/users'));
 
 class App extends Component {
   constructor() {
@@ -30,27 +32,29 @@ class App extends Component {
       <Router>
         <main className="App">
           <Header/>
-          <section>
-            <div className="container">
-              <Switch>
-                {/* <Redirect exact from="/" to="/Products"/> */}
-                <Route path="/" exact component={Products} />
-                <Route path="/News" component={News}/>
-                <Route path="/First" render={(props) => <First {...props} name="World" /> } />
-                <Route path="/Second" render={(props) => <Second {...props} title="this is title" content="this is content" /> } />
-                <Route path="/Third" component={Third} />
-                <Route path="/Api1" component={Api1} />
-                <Route path="/TodoApp" component={TodoApp} />
-                <Route path="/InputOutput" component={InputOutput} />
-                <Route path="/Lists" component={Lists} />
-                <Route path="/Users" component={users} />
-                <Route path='/course/:id' exact component={course} />
-                <Route path="/Courses" component={courses} />
-                <Route path="/iss" component={iss} />
-                <Route render={() => <div>Sorry, page is not found</div>}/>
-              </Switch>
-            </div>
-          </section>
+          <Suspense fallback={<div>loading....</div>}>
+            <section>
+              <div className="container">
+                <Switch>
+                  {/* <Redirect exact from="/" to="/Products"/> */}
+                  <Route path="/" exact component={Products} />
+                  <Route path="/News" component={News}/>
+                  <Route path="/First" render={(props) => <First {...props} name="World" /> } />
+                  <Route path="/Second" render={(props) => <Second {...props} title="this is title" content="this is content" /> } />
+                  <Route path="/Third" component={Third} />
+                  <Route path="/Api1" component={Api1} />
+                  <Route path="/TodoApp" component={TodoApp} />
+                  <Route path="/InputOutput" component={InputOutput} />
+                  <Route path="/Lists" component={Lists} />
+                  <Route path="/Users" component={users} />
+                  <Route path='/course/:id' exact component={course} />
+                  <Route path="/Courses" component={courses} />
+                  <Route path="/iss" component={iss} />
+                  <Route render={() => <div>Sorry, page is not found</div>}/>
+                </Switch>
+              </div>
+            </section>
+          </Suspense>
         </main>
       </Router>
     );
